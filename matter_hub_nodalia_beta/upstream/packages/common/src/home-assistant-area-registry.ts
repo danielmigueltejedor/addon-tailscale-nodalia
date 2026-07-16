7d8f59feeada54fa955136852dd489865e1a0958
Daniel <danielmigueltejedor@icloud.com>
2026-07-16 02:48:46 +0200

    Delete matter_hub_nodalia_beta directory

--- matter_hub_nodalia_beta/upstream/packages/common/src/home-assistant-area-registry.ts
+++
@@ -1,9 +0,0 @@
-export interface HomeAssistantAreaRegistry {
-  area_id: string;
-  name: string;
-  picture?: string | null;
-  floor_id?: string | null;
-  icon?: string | null;
-  labels?: string[];
-  aliases?: string[];
-}
